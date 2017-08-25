01434fc7686198e5c71ba036d4626639e418efb2
Emilie G <adamanska@gmail.com>
2017-08-25 14:12:58 +0200

    slots

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote>
+                   <h2>{{ quoteTitle }}</h2>
+                   <p>You may delay but time will not</p>
+               </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './comp/Quote.vue';
+
+    export default {
+        data: function () {
+            return {
+                quoteTitle: "The quote title passed in data"
+            }
+        },
+        components: {
+            appQuote: Quote // nécessaire pour passer app-quote ici
+        }
+    }
+</script>
+
+<style>
+</style>
